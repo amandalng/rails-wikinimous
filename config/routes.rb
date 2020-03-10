653176bd99025b7a57b192240c056f772587a40d
Amanda Ng <amandaleahng@gmail.com>
2020-03-10 15:41:53 +0800

    wikinimous completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
+  # get 'articles/new'
+  # get 'articles/create'
+  # get 'articles/index'
+  # get 'articles/show'
+  # get 'articles/edit'
+  # get 'articles/update'
+  # get 'articles/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+# create methods - new, create
+
+get 'articles/new', to: 'articles#new'
+post 'articles', to: 'articles#create'
+
+# read methods - index, show
+get 'articles', to: 'articles#index'
+get 'articles/:id', to: 'articles#show', as: 'article'
+
+# update methods - edit, update
+get 'articles/:id/edit', to: 'articles#edit', as: 'edit_article'
+patch 'articles/:id', to: 'articles#update'
+
+# delete methods - destroy
+delete 'articles/:id', to: 'articles#destroy'
 end
